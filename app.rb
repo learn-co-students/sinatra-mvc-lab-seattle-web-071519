bed6ab02887838493d31c0861b92c072d8599622
marshallhelenm <marshallhelenm@gmail.com>
2019-08-06 11:06:47 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        p1 = PigLatinizer.new
+        @pigged = p1.piglatinize(params[:user_phrase])
+        erb :results
+    end
 end
